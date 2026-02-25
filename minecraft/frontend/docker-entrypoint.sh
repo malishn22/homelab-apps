2acc9bf7e83e975464c7de7d1aa93b026bbad507
Mali <mali.sahin2222@gmail.com>
2026-02-26 02:00:03 +0300

    Added dev docker compose and updated structure for frontend

---
+++ minecraft/frontend/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+: "${MINECRAFT_API_PORT:=8000}"
+: "${MINECRAFT_WEB_INTERNAL_PORT:=80}"
+cat > /etc/nginx/conf.d/default.conf << EOF
+server {
+    listen ${MINECRAFT_WEB_INTERNAL_PORT};
+    server_name _;
+    root /usr/share/nginx/html;
+    index index.html;
+    location /api/ {
+        proxy_pass http://${BACKEND_HOST:-minecraft-api}:${MINECRAFT_API_PORT};
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+    }
+    location / {
+        try_files \$uri \$uri/ /index.html;
+    }
+}
+EOF
+exec nginx -g 'daemon off;'
